520b53b504cbbe406e9b081f47a01b9ef296cd58
Charles Park <charleshypk@gmail.com>
2016-11-29 23:38:37 -0500

    Solution for question 2.

--- ctci/chapter1/Solution.java
+++ ctci/chapter1/Solution.java
@@ -1,7 +1,7 @@
 public class Solution {
 
     public static void main(String args[]) {
-        
+
     }
 
     // Question 1
@@ -17,5 +17,23 @@ public class Solution {
         }
         return true;
     }
-    
+
+    // Question 2
+    public static boolean isPermutation(String str1, String str2) {
+        if (str1.length() != str2.length()) return false;
+
+        int[] letters = new int[128];
+        for (char c : str1.toCharArray()) {
+            letters[c]++;
+        }
+
+        for (char c : str2.toCharArray()) {
+            letters[c]--;
+            if (letters[c] < 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
 }
