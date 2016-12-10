a5d6066e068b7dc9cd1a9cd4675468f50c1e10e8
Charles Park <charleshypk@gmail.com>
2016-12-10 15:38:31 -0500

    Solution for question 9.

--- ctci/chapter1/Solution.java
+++ ctci/chapter1/Solution.java
@@ -169,4 +169,10 @@ public class Solution {
         }
     }
 
+    // Question 9
+    public static boolean isStringRotation(String str1, String str2) {
+        if (str1.length() != str2.length()) return false;
+        return (str1 + str1).contains(str2);
+    }
+
 }
